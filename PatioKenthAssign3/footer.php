44775f17f709b167df6239ec5fa7049bd1d4a28d
kenthandreipatio <kenthandreipatio@gmail.com>
2025-12-03 19:10:05 +0800

    Add files via upload

---
+++ PatioKenthAssign3/footer.php
@@ -0,0 +1,4 @@
+<!-- footer.php -->
+<footer>
+    <p>KENTH ANDREI M. PATIO | CYB-201 | 6DWEB | DECEMBER 03 2025</p>
+</footer>
---
+++ PatioKenthAssign3/header.php
@@ -0,0 +1,13 @@
+<header>
+    <div class="header-container">
+        <h1>F1 STORE</h1>
+        <nav>
+            <ul>
+                <li><a href="store.php">Home</a></li>
+                <li><a href="#">Products</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
